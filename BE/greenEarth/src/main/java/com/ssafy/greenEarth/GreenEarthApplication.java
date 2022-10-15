a948215c9a08a47ca4b7ed138a4c583e04b8ee05
jongil512 <jongil519@gmail.com>
2022-10-15 17:14:23 +0900

    BackEnd Dev Env setting

--- BE/greenEarth/src/main/java/com/ssafy/greenEarth/GreenEarthApplication.java
+++ BE/greenEarth/src/main/java/com/ssafy/greenEarth/GreenEarthApplication.java
@@ -1,13 +1,13 @@
-package com.greenEarth.demo;
+package com.ssafy.greenEarth;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DemoApplication {
+public class GreenEarthApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
+		SpringApplication.run(GreenEarthApplication.class, args);
 	}
 
 }
