b38602564378b12d504e64c035b866dd9d0f320e
unknown <ict.cspark@gmail.com>
2022-10-20 04:55:07 +0900

    [S07P31D206-147] test - React CRUD by Recoil

--- FE/PCS/emotion-diary/src/pages/Edit.js
+++ FE/PCS/emotion-diary/src/pages/Edit.js
@@ -1,27 +1,42 @@
-import { useNavigate, useSearchParams } from "react-router-dom";
+import { useEffect, useState } from "react";
+import { useNavigate, useParams } from "react-router-dom";
+
+import { useRecoilState } from "recoil";
+import { diaryListState } from "./../store/atoms";
+
+import DiaryEditor from "../components/DiaryEditor";
 
 const Edit = () => {
   const navigate = useNavigate();
-  const [searchParams, setSearchParams] = useSearchParams();
 
-  const id = searchParams.get("id");
-  console.log("id :", id);
+  const [diaryList, setDiaryList] = useRecoilState(diaryListState);
+
+  const { id } = useParams();
+  const [originData, setOriginData] = useState();
+
+  useEffect(() => {
+    const titleElement = document.getElementsByTagName("title")[0];
+    titleElement.innerHTML = `감정 일기장 - ${id}번 일기 수정`;
+  }, []);
+
+  useEffect(() => {
+    if (diaryList.length >= 1) {
+      const targetDiary = diaryList.find(
+        (it) => parseInt(it.id) === parseInt(id)
+      );
+
+      if (targetDiary) {
+        setOriginData(targetDiary);
+      } else {
+        alert("존재하지 않는 일기입니다.");
+        navigate("/", { replace: true });
+      }
+    }
+  }, [id, diaryList]);
 
   return (
     <div>
-      <h1>Edit</h1>
-      <p>이곳은 Edit 입니다.</p>
-      <button onClick={() => setSearchParams({ who: "winterlood" })}>
-        qs 바꾸기
-      </button>
-
-      <button
-        onClick={() => {
-          navigate("/");
-        }}
-      >
-        Home으로 가기
-      </button>
+      {originData && <DiaryEditor isEdit={true} originData={originData} />}
     </div>
   );
 };
