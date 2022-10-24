1931cd332400f42bd81a405f38f7864c21b412a0
unknown <ict.cspark@gmail.com>
2022-10-25 07:11:21 +0900

    [S07P31D206-249] front - ReactSetting

--- FE/green-earth/src/App.js
+++ FE/green-earth/src/App.js
@@ -1,14 +1,32 @@
 import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-import Home from "./pages/Home";
+import WelcomePage from "./pages/WelcomePage";
+import LogInPage from "./pages/LogInPage";
+import SignUpPage from "./pages/SignUpPage";
+import EditAccountPage from "./pages/EditAccountPage";
+import ChildMainPage from "./pages/ChildMainPage";
+import ParentMainPage from "./pages/ParentMainPage";
+import ProfilePage from "./pages/ProfilePage";
+import GamePage from "./pages/GamePage";
+import GamePicturePage from "./pages/GamePicturePage";
+import GameRecyclePage from "./pages/GameRecyclePage";
 
 function App() {
   return (
     <BrowserRouter>
       <div className="App">
         <Routes>
-          <Route path="/" element={<Home />} />
+          <Route path="/" element={<WelcomePage />} />
+          <Route path="/login" element={<LogInPage />} />
+          <Route path="/signup" element={<SignUpPage />} />
+          <Route path="/account/:child_id" element={<EditAccountPage />} />
+          <Route path="/child" element={<ChildMainPage />} />
+          <Route path="/parent" element={<ParentMainPage />} />
+          <Route path="/profile/:child_id" element={<ProfilePage />} />
+          <Route path="/game" element={<GamePage />} />
+          <Route path="/game/picture" element={<GamePicturePage />} />
+          <Route path="/game/recycle" element={<GameRecyclePage />} />
         </Routes>
       </div>
     </BrowserRouter>
