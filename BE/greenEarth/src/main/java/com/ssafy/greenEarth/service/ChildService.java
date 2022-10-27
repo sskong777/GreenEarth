051ac127326b909bd9de8be0e77bb38176c3891e
Jieun <skysun704@gmail.com>
2022-10-27 14:50:50 +0900

    [S07P31D206-214] feat : create child service find method

--- BE/greenEarth/src/main/java/com/ssafy/greenEarth/service/ChildService.java
+++ BE/greenEarth/src/main/java/com/ssafy/greenEarth/service/ChildService.java
@@ -2,6 +2,7 @@ package com.ssafy.greenEarth.service;
 
 import com.ssafy.greenEarth.domain.Child;
 import com.ssafy.greenEarth.domain.Parent;
+import com.ssafy.greenEarth.dto.ChildProfileDto;
 import com.ssafy.greenEarth.dto.ChildRegisterDto;
 import com.ssafy.greenEarth.repository.ChildRepository;
 import com.ssafy.greenEarth.repository.ParentRepository;
@@ -20,11 +21,18 @@ public class ChildService {
 
     private final ParentRepository parentRepository;
 
+    public ChildProfileDto findChild(int childId) {
+        // 아이 프로필 조회
+        Child child = childRepository.findById(childId)
+                .orElseThrow(() -> new IllegalArgumentException(String.format("해당 아이 프로필을 찾을 수 없습니다.")));
+        return new ChildProfileDto(child);
+    }
+
     @Transactional
     public Child registerChild(ChildRegisterDto childDto) {
-        // 연결된 보호자 찾기
+        // 연결된 보호자 조회
         Parent parent = parentRepository.findById(1)
-                .orElseThrow(() -> new IllegalAccessError(String.format("연결될 보호자 계정을 찾을 수 없습니다.")));
+                .orElseThrow(() -> new IllegalArgumentException(String.format("연결될 보호자 계정을 찾을 수 없습니다.")));
         // 등록
         return childRepository.save(childDto.toEntity(parent));
     }
