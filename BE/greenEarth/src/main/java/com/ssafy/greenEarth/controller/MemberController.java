f7ee27e8cf9c16a979b19263c4c853db8acc3923
정종일 <jongil519@gmail.com>
2022-10-27 17:56:10 +0900

    refactor - deploy 변경

--- BE/greenEarth/src/main/java/com/ssafy/greenEarth/controller/MemberController.java
+++ BE/greenEarth/src/main/java/com/ssafy/greenEarth/controller/MemberController.java
@@ -1,17 +1,20 @@
 package com.ssafy.greenEarth.controller;
 
 import com.ssafy.greenEarth.domain.Child;
-import com.ssafy.greenEarth.dto.ChildProfileDto;
-import com.ssafy.greenEarth.dto.ChildRegisterDto;
+import com.ssafy.greenEarth.dto.Child.ChildProfileDto;
+import com.ssafy.greenEarth.dto.Child.ChildRegisterDto;
 import com.ssafy.greenEarth.dto.ResponseDto;
 import com.ssafy.greenEarth.service.ChildService;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+@Api("MemberController")
 @Slf4j
 @RestController
 @RequestMapping("/member")
@@ -23,17 +26,16 @@ public class MemberController {
 
     private final ChildService childService;
 
-    // 아이 프로필 조회
+    @ApiOperation(value = "아이 프로필 조회", notes = "아이 ID 받아서 프로필 전달")
     @GetMapping("/child/{childId}")
     public ResponseDto profile(@PathVariable int childId) {
         ChildProfileDto data = childService.findChild(childId);
         return new ResponseDto(data);
     }
 
-    // 일반 회원가입
+    @ApiOperation(value = "아이 등록", notes = "아이 등록 및 현재 접속 중인 보호자 계정과 연결")
     @PostMapping("/signup")
     public ResponseEntity<String> childSignup(@RequestBody ChildRegisterDto childDto) {
-        System.out.println("회원가입 요청: " + childDto.getNickname());
         Child child = childService.registerChild(childDto);
         if (child != null){
             return new ResponseEntity<>(SUCCESS, HttpStatus.OK);
