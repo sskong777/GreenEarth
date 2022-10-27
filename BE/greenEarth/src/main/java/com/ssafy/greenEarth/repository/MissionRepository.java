d294c4e4604c5ceb0756d6186aedf9c9309b57b2
SSAFY <sskong777@gmail.com>
2022-10-27 22:20:32 +0900

    [S07P31D206-217] feat : modify exception

--- BE/greenEarth/src/main/java/com/ssafy/greenEarth/repository/MissionRepository.java
+++ BE/greenEarth/src/main/java/com/ssafy/greenEarth/repository/MissionRepository.java
@@ -5,9 +5,13 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
+import java.util.Optional;
+
 public interface MissionRepository extends JpaRepository<Mission, Integer> {
 
 //    @Query("select m from Mission m join fetch m.missionLogList ml  where m.id = :id")
     Mission findById(int id);
+
+    Optional<Mission> findByMissionId(int id);
 
 }
