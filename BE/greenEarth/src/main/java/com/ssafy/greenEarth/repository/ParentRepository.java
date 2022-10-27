2685b1d8e136c9e2676c00cebc65b5d095ccac18
Jieun <skysun704@gmail.com>
2022-10-27 13:08:23 +0900

    [S07P31D206-191] feat : create parent repository

---
+++ BE/greenEarth/src/main/java/com/ssafy/greenEarth/repository/ParentRepository.java
@@ -0,0 +1,7 @@
+package com.ssafy.greenEarth.repository;
+
+import com.ssafy.greenEarth.domain.Parent;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ParentRepository extends JpaRepository<Parent, Integer> {
+}
