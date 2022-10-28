ab4e22041766b417841bc6857956f660c32a77c3
정종일 <jongil519@gmail.com>
2022-10-28 10:05:25 +0900

    refactor - yml 설정 변경

--- BE/greenEarth/src/main/java/com/ssafy/greenEarth/dto/MissionResDto.java
+++ BE/greenEarth/src/main/java/com/ssafy/greenEarth/dto/MissionResDto.java
@@ -1,18 +1,12 @@
 package com.ssafy.greenEarth.dto;
 
-import com.fasterxml.jackson.annotation.JsonBackReference;
 import com.ssafy.greenEarth.domain.Mission;
-import com.ssafy.greenEarth.domain.MissionLog;
 import lombok.Data;
-
-import javax.persistence.*;
-import java.util.ArrayList;
-import java.util.List;
 
 @Data
 public class MissionResDto {
 
-    private int id;
+    private int missionId;
 
     private String name;
 
@@ -21,7 +15,7 @@ public class MissionResDto {
     private int mileage;
 
     public  MissionResDto(Mission mission){
-        this.id = mission.getId();
+        this.missionId = mission.getId();
         this.name = mission.getName();
         this.description = mission.getDescription();
         this.mileage = mission.getMileage();
---
+++ BE/greenEarth/src/main/java/com/ssafy/greenEarth/dto/RewardPutDto.java
@@ -0,0 +1,15 @@
+package com.ssafy.greenEarth.dto;
+
+import lombok.Data;
+
+@Data
+public class RewardPutDto {
+
+
+    private String rewardName;
+
+    private int rewardCondition;
+
+    private int childId;
+
+}
