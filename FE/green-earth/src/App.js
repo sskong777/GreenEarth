65e47e25a543076509f4d43cd3543663e9dfec63
강지명 <rainbow7417@naver.com>
2022-10-28 17:32:39 +0900

    [S07P31D206] feature - 아이메인페이지 작성
    Former-commit-id: 345c733e934bd477b3d1b4bab62f93340ba3aaec

--- FE/green-earth/src/App.js
+++ FE/green-earth/src/App.js
@@ -1,6 +1,12 @@
-import "./App.css";
+import "./style/App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
+<<<<<<< HEAD
+import Home from "./pages/Home";
+import ChildMainPage from "./pages/ChildMainPage"
+import GamePage from "./pages/GamePage"
+import ParentMainPage from "./pages/ParentMainPage"
+=======
 import WelcomePage from "./pages/WelcomePage";
 import LogInPage from "./pages/LogInPage";
 import SignUpPage from "./pages/SignUpPage";
@@ -11,12 +17,20 @@ import ProfilePage from "./pages/ProfilePage";
 import GamePage from "./pages/GamePage";
 import GamePicturePage from "./pages/GamePicturePage";
 import GameRecyclePage from "./pages/GameRecyclePage";
+>>>>>>> d5840b42ca456e03d06f4d115ab9b2fc4d592f5f
 
 function App() {
+
   return (
     <BrowserRouter>
       <div className="App">
         <Routes>
+<<<<<<< HEAD
+          <Route path="/" element={<Home />} />
+          <Route path="child" element={<ChildMainPage />}/>
+          <Route path="game" element={<GamePage />}/>
+          <Route path="parent" element={<ParentMainPage />}/>
+=======
           <Route path="/" element={<WelcomePage />} />
           <Route path="/login" element={<LogInPage />} />
           <Route path="/signup" element={<SignUpPage />} />
@@ -27,6 +41,7 @@ function App() {
           <Route path="/game" element={<GamePage />} />
           <Route path="/game/picture" element={<GamePicturePage />} />
           <Route path="/game/recycle" element={<GameRecyclePage />} />
+>>>>>>> d5840b42ca456e03d06f4d115ab9b2fc4d592f5f
         </Routes>
       </div>
     </BrowserRouter>
