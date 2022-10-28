707c36edd2f5f05c3c40bc22a1a14a3d7dc87448
unknown <ict.cspark@gmail.com>
2022-10-28 20:21:19 +0900

    fix - front 충돌 수정
    Former-commit-id: b7ff1db611a6e0396a0ea9ba57af7dee2b0ad2e6

--- FE/green-earth/src/App.js
+++ FE/green-earth/src/App.js
@@ -1,47 +1,32 @@
-import "./style/App.css";
+import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-
-
-
-import ChildMainPage from "./pages/ChildMainPage"
-import GamePage from "./pages/GamePage"
-import ParentMainPage from "./pages/ParentMainPage"
 
 import WelcomePage from "./pages/WelcomePage";
 import LogInPage from "./pages/LogInPage";
 import SignUpPage from "./pages/SignUpPage";
 import EditAccountPage from "./pages/EditAccountPage";
-
-
+import ChildMainPage from "./pages/ChildMainPage";
+import ParentMainPage from "./pages/ParentMainPage";
 import ProfilePage from "./pages/ProfilePage";
-
+import GamePage from "./pages/GamePage";
 import GamePicturePage from "./pages/GamePicturePage";
 import GameRecyclePage from "./pages/GameRecyclePage";
 
-
 function App() {
-
   return (
     <BrowserRouter>
       <div className="App">
         <Routes>
-
-      
-          <Route path="child" element={<ChildMainPage />}/>
-          <Route path="game" element={<GamePage />}/>
-          <Route path="parent" element={<ParentMainPage />}/>
-
           <Route path="/" element={<WelcomePage />} />
           <Route path="/login" element={<LogInPage />} />
           <Route path="/signup" element={<SignUpPage />} />
-          <Route path="/account/:childId" element={<EditAccountPage />} />
+          <Route path="/account/:child_id" element={<EditAccountPage />} />
           <Route path="/child" element={<ChildMainPage />} />
           <Route path="/parent" element={<ParentMainPage />} />
-          <Route path="/profile/:childId" element={<ProfilePage />} />
+          <Route path="/profile/:child_id" element={<ProfilePage />} />
           <Route path="/game" element={<GamePage />} />
           <Route path="/game/picture" element={<GamePicturePage />} />
           <Route path="/game/recycle" element={<GameRecyclePage />} />
-
         </Routes>
       </div>
     </BrowserRouter>
