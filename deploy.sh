9e9ce5f23b5654200c21737a0bec7df9c98505e1
jongil512 <jongil519@gmail.com>
2022-10-28 21:16:47 +0900

    refactor - 서버 배포 설정 추가
    Former-commit-id: a2be5f9eed19897b01be8d75861f9688a4869a9e

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-DOCKER_APP_NAME="greenEarth_backend"
+DOCKER_APP_NAME="greenearth_backend"
 
 # 현재 떠있는 컨테이너를 체크한다.
 EXIST=$(docker-compose -p ${DOCKER_APP_NAME} -f docker-compose.yml ps | grep Up)
@@ -13,11 +13,11 @@ if [ -z "$EXIST" ]; then
     docker-compose -p ${DOCKER_APP_NAME} -f docker-compose.yml down
 
     # 이전 이미지 삭제
-    docker rmi greenEarth_backend:0.1
+    docker rmi greenearth_backend:0.1
 fi
 
     echo "server up"
-    docker-compose -p ${DOCKER_APP_NAME} -f docker-compose.blue.yml up -d
+    docker-compose -p ${DOCKER_APP_NAME} -f docker-compose.yml up -d
 
 sleep 8
 
