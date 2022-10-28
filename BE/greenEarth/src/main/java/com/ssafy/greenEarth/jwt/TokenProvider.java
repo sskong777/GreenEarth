84344444dd9f348c8393d982e36485f84e73e725
박지은 <skysun704@gmail.com>
2022-10-28 10:21:42 +0900

    merge : refresh token entity

--- BE/greenEarth/src/main/java/com/ssafy/greenEarth/jwt/TokenProvider.java
+++ BE/greenEarth/src/main/java/com/ssafy/greenEarth/jwt/TokenProvider.java
@@ -16,7 +16,7 @@ import java.util.Date;
 @Component
 public class TokenProvider {
 
-    private final String secretKey;
+    private final String SECRET_KEY;
 
     private final long accessTokenValidityInMilliseconds;
 
@@ -24,16 +24,15 @@ public class TokenProvider {
 
     public TokenProvider(
             @Value("${jwt.secretKey}") String secretKey) {
-        this.secretKey = Base64.getEncoder().encodeToString(secretKey.getBytes());  // 비밀키 Base64로 인코딩하여 설정
+        this.SECRET_KEY = Base64.getEncoder().encodeToString(secretKey.getBytes());  // 비밀키 Base64로 인코딩하여 설정
         this.accessTokenValidityInMilliseconds = JwtProperties.accessTokenValidityInSeconds * 1000;
         this.refreshTokenValidityInMilliseconds = JwtProperties.refreshTokenValidityInSeconds * 1000;
     }
 
     // 토큰 생성 메소드
     public String createAccessToken(int subject, Role role) {
-
-        // Claim : JWT payload 에 저장되는 속성 정보 단위 (java 에서는 Json map 형식의 인터페이스)
-        Claims claims = Jwts.claims().setSubject(Integer.toString(subject));     // 토큰 제목 (sub)
+        Claims claims = Jwts.claims();
+        claims.put("Id", subject);
         claims.put("Role", role);
 
         Date now = new Date();
@@ -43,24 +42,43 @@ public class TokenProvider {
         return Jwts.builder()
                 .setClaims(claims)
                 .setIssuedAt(now)                               // 발급 시간 (iat)
+                .setSubject("accessToken")                      // 토큰 제목 (sub)
                 .setExpiration(accessTokenExpiresIn)            // 만료 시간 (exp)
-                .signWith(SignatureAlgorithm.HS256, secretKey)  // 암호화 알고리즘, 시크릿키
+                .signWith(SignatureAlgorithm.HS256, SECRET_KEY)  // 암호화 알고리즘, 시크릿키
                 .compact();
     }
 
     public String createRefreshToken(int subject, Role role) {
-        // Claim : JWT payload 에 저장되는 속성 정보 단위 (java 에서는 Json map 형식의 인터페이스)
-        Claims claims = Jwts.claims().setSubject(Integer.toString(subject));     // 토큰 제목 (sub)
-        claims.put("Role", role);
-
         Date now = new Date();
         Date refreshTokenExpiresIn = new Date(now.getTime() + this.refreshTokenValidityInMilliseconds);   // refresh token 만료 시간 설정
 
         // Refresh Token 생성
         return Jwts.builder()
+                .setSubject("refreshToken")
                 .setExpiration(refreshTokenExpiresIn)
-                .signWith(SignatureAlgorithm.HS256, secretKey)
+                .signWith(SignatureAlgorithm.HS256, SECRET_KEY)
                 .compact();
     }
+
+    // claim 정보 (payload data) 추출
+    public Claims getClaims(String jwtToken) {
+        return Jwts.parser().setSigningKey(SECRET_KEY.getBytes()).parseClaimsJws(jwtToken).getBody();
+    }
+
+    // 토큰 만료여부 체크
+    public boolean isTokenExpired(String jwtToken) {
+        try {
+            return !getClaims(jwtToken).getExpiration().before(new Date());
+        } catch (Exception e) {
+            return false;
+        }
+    }
+
+    // 토큰 유효성 검층
+//    public boolean isTokenValid(String jwtToken) {
+//        try {
+//            getClaims(jwtToken).getId();
+//        }
+//    }
 
 }
