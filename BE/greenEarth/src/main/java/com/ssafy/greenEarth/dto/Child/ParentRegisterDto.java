7976e9709f6493c9dab292b920b21a30b98b0854
jongil512 <jongil519@gmail.com>
2022-10-30 22:26:37 +0900

    [S07P31D206-185] feat - 카카오 로그인 구현
    Former-commit-id: e1b6b2b202d6effc2d51570f0605278d7b590091

---
+++ BE/greenEarth/src/main/java/com/ssafy/greenEarth/dto/Child/ParentRegisterDto.java
@@ -0,0 +1,25 @@
+package com.ssafy.greenEarth.dto.Child;
+
+import com.ssafy.greenEarth.domain.Parent;
+import com.ssafy.greenEarth.domain.Role;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+public class ParentRegisterDto {
+
+    private String email;
+
+    private String nickname;
+
+    private Role role;
+
+    public Parent toEntity() {
+        return Parent.builder()
+                .email(this.getEmail())
+                .nickname(this.getNickname())
+                .role(Role.ROLE_PARENT)
+                .build();
+    }
+}
