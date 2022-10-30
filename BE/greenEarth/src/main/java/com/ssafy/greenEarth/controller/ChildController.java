b814b1328cc12348b3a7e16a0ffb61070fba603e
정종일 <jongil519@gmail.com>
2022-10-30 19:45:59 +0900

    merge - 회원 프로필 조회 & 회원 탈퇴 & 게임 마일리지 획득 구현
    Former-commit-id: c55db02eabeecdb98d6157775c7d39206335ca52

--- BE/greenEarth/src/main/java/com/ssafy/greenEarth/controller/ChildController.java
+++ BE/greenEarth/src/main/java/com/ssafy/greenEarth/controller/ChildController.java
@@ -5,6 +5,7 @@ import com.ssafy.greenEarth.domain.Role;
 import com.ssafy.greenEarth.dto.Child.ChildProfileDto;
 import com.ssafy.greenEarth.dto.Child.ChildUpdateDto;
 import com.ssafy.greenEarth.dto.Child.ChildRegisterDto;
+import com.ssafy.greenEarth.dto.Child.ParentProfileDto;
 import com.ssafy.greenEarth.dto.ResponseDto;
 import com.ssafy.greenEarth.service.ChildService;
 
@@ -32,9 +33,23 @@ public class ChildController {
 
     @ApiOperation(value = "아이 프로필 조회", notes = "아이 ID 받아서 프로필 전달")
     @GetMapping("/child/{childId}")
-    public ResponseDto getProfile(@PathVariable int childId) {
+    public ResponseDto getChildProfile(@PathVariable int childId) {
         ChildProfileDto data = childService.findChild(childId);
         return new ResponseDto(data);
+    }
+
+    @ApiOperation(value = "본인 회원 정보 조회", notes = "토큰 ID 받아서 프로필 전달")
+    @GetMapping("/member")
+    public ResponseDto getProfile(HttpServletRequest request) {
+        int curUserId = (int) request.getAttribute("curUserId");
+        Role curUserRole = (Role) request.getAttribute("curUserRole");
+        if (curUserRole == Role.ROLE_PARENT) {
+            ParentProfileDto data = childService.findParent(curUserId);
+            return new ResponseDto(data);
+        }else {
+            ChildProfileDto data = childService.findChild(curUserId);
+            return new ResponseDto(data);
+        }
     }
 
     @ApiOperation(value = "아이 등록", notes = "아이 등록 및 현재 접속 중인 보호자 계정과 연결")
@@ -50,11 +65,18 @@ public class ChildController {
 
     @ApiOperation(value = "아이 닉네임 수정", notes = "아이 nickname 수정 후 아이 프로필 정보 전달")
     @PutMapping("/child/{childId}")
-    public ResponseDto updateProfile(@PathVariable int childId, @RequestBody ChildUpdateDto childDto, HttpServletRequest request) {
-        int curUserId = (int) request.getAttribute("curUserId");
-        Role curUserRole = (Role) request.getAttribute("curUserRole");
+    public ResponseDto updateProfile(@PathVariable int childId, @RequestBody ChildUpdateDto childDto) {
         ChildProfileDto data = childService.updateProfile(childId, childDto);
         return new ResponseDto(data);
+    }
+
+    @ApiOperation(value = "회원 탈퇴", notes = "토큰 ID 받아서 회원 탈퇴")
+    @DeleteMapping
+    public void deleteProfile(HttpServletRequest request) {
+        log.info("delete controller 실행");
+        int curUserId = (int) request.getAttribute("curUserId");
+        Role curUserRole = (Role) request.getAttribute("curUserRole");
+        childService.deleteProfile(curUserId, curUserRole);
     }
 
 }
