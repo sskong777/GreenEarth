b0dd8ccb025f193fcf233cbb50412057983103b7
jongil512 <jongil519@gmail.com>
2022-10-31 16:22:50 +0900

    release - 배포 설정 변경
    Former-commit-id: 7b22003c721fd8c66fd5455f22be0b02e31231da

--- deploy.sh
+++ deploy.sh
@@ -10,18 +10,18 @@ docker-compose -p ${DOCKER_APP_BACK_NAME} -f docker-compose-back.yml down
 # 이전 이미지 삭제
 docker rmi greenearth_backend:0.1
 
-# frontend 컨테이너 down
-echo "Frontend down"
-docker-compose -p ${DOCKER_APP_FRONT_NAME} -f docker-compose-front.yml down
+# # frontend 컨테이너 down
+# echo "Frontend down"
+# docker-compose -p ${DOCKER_APP_FRONT_NAME} -f docker-compose-front.yml down
 
-# 이전 이미지 삭제
-docker rmi greenearth_front:0.1
+# # 이전 이미지 삭제
+# docker rmi greenearth_front:0.1
 
 echo "Backend up"
 docker-compose -p ${DOCKER_APP_BACK_NAME} -f docker-compose-back.yml up -d
 
-echo "Frontend down"
-docker-compose -p ${DOCKER_APP_FRONT_NAME} -f docker-compose-front.yml up -d
+# echo "Frontend down"
+# docker-compose -p ${DOCKER_APP_FRONT_NAME} -f docker-compose-front.yml up -d
 
 sleep 5
 
