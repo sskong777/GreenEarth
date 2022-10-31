d4b2dc77d73ce82e5bd6c442fbba9d3ba1a7baf7
GyeongYeol <hrq789@naver.com>
2022-10-31 19:32:22 +0900

    [S07P31D206-291] feature - 로그인 페이지 작성
    Former-commit-id: b9a5336b83b76f0b4aa6fee1aa61b4eb5f496724

--- FE/green-earth/src/functions/useAuthCallback.js
+++ FE/green-earth/src/functions/useAuthCallback.js
@@ -1,13 +1,41 @@
 import { useRecoilState } from "recoil";
-import { ChildrenInfoState, ChildInfoState } from "../store/atoms";
+import { jwtTokenState } from "../store/LoginStore";
 
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
 
-const useAuthCallback = () => {
+export const useAuthCallback = () => {
   const navigate = useNavigate();
 
-  return;
-};
+  const [jwtToken, setJwtToken] = useRecoilState(jwtTokenState);
 
-export default useAuthCallback;
+  const loginCallback = async (email, password) => {
+    axios({
+      method: "post",
+      url: "/api/member/login/child",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      data: {
+        email,
+        password,
+      },
+    })
+      .then((response) => {
+        if (response.data) {
+          console.log(response.data);
+          console.log("로그인되었습니다.");
+          setJwtToken(response.data.token);
+          // userInfoCallback(response.data.token)
+          // profileListCallback(response.data.token)
+        }
+      })
+      .catch((error) => {
+        console.log(error.response.data);
+        // setModalContent(error.response.data?.Messege ? error.response.data?.Messege : '오류가 발생했습니다.')
+        // setModalOpen(true)
+      });
+  };
+
+  return { loginCallback };
+};
