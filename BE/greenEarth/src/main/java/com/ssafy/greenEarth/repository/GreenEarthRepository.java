1551844177033e1f5358ce7b5111c68dc2a0deb8
Jieun <skysun704@gmail.com>
2022-11-01 17:00:39 +0900

    [S07P31D206-358] fix query error
    Former-commit-id: beea9166d13a32798bcc099f22b5a0c194824b89

--- BE/greenEarth/src/main/java/com/ssafy/greenEarth/repository/GreenEarthRepository.java
+++ BE/greenEarth/src/main/java/com/ssafy/greenEarth/repository/GreenEarthRepository.java
@@ -7,10 +7,10 @@ import org.springframework.data.repository.query.Param;
 
 public interface GreenEarthRepository extends JpaRepository<GreenEarth, Integer> {
 
-    @Query(value = "SELECT g.green_earth_id" +
-            "FROM green_earth g" +
-            "WHERE g.mileage_condition <= :mileage" +
-            "ORDER BY g.green_earth_id" +
+    @Query(value = "SELECT g.green_earth_id " +
+            "FROM green_earth g " +
+            "WHERE g.mileage_condition <= :mileage " +
+            "ORDER BY g.green_earth_id " +
             "DESC LIMIT 1",
             nativeQuery = true)
     int findFirstByMileage_condition(@Param("mileage") int mileage);
