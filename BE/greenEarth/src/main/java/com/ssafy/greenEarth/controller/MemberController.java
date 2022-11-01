59dd0f9ef4e254a28fc7597e7b2b25b02d6cd745
Jieun <skysun704@gmail.com>
2022-11-01 13:59:43 +0900

    [S07P31D206-334] refactor - response structure modified
    Former-commit-id: 107dfa4c1e0430c848c219a45c3664a2b70b83a5

--- BE/greenEarth/src/main/java/com/ssafy/greenEarth/controller/MemberController.java
+++ BE/greenEarth/src/main/java/com/ssafy/greenEarth/controller/MemberController.java
@@ -33,35 +33,31 @@ public class MemberController {
 
     @ApiOperation(value = "아이 프로필 조회", notes = "아이 ID 받아서 프로필 전달")
     @GetMapping("/child/{childId}")
-    public ResponseDto getChildProfile(@PathVariable int childId) {
+    public ResponseEntity<ChildProfileDto> getChildProfile(@PathVariable int childId) {
         ChildProfileDto data = childService.findChild(childId);
-        return new ResponseDto(data);
+        return new ResponseEntity<>(data, HttpStatus.OK);
     }
 
     @ApiOperation(value = "본인 회원 정보 조회", notes = "토큰 ID 받아서 프로필 전달")
     @GetMapping("/member")
-    public ResponseDto getProfile(HttpServletRequest request) {
+    public ResponseEntity<?> getProfile(HttpServletRequest request) {
         int curUserId = (int) request.getAttribute("curUserId");
         Role curUserRole = (Role) request.getAttribute("curUserRole");
         if (curUserRole == Role.ROLE_PARENT) {
             ParentProfileDto data = childService.findParent(curUserId);
-            return new ResponseDto(data);
+            return new ResponseEntity<>(data, HttpStatus.OK);
         }else {
             ChildProfileDto data = childService.findChild(curUserId);
-            return new ResponseDto(data);
+            return new ResponseEntity<>(data, HttpStatus.OK);
         }
     }
 
     @ApiOperation(value = "아이 등록", notes = "아이 등록 및 현재 접속 중인 보호자 계정과 연결")
     @PostMapping("/signup")
-    public ResponseEntity<String> signup(@RequestBody ChildRegisterDto childDto, HttpServletRequest request) {
+    public ResponseEntity<Child> signup(@RequestBody ChildRegisterDto childDto, HttpServletRequest request) {
         int curUserId = (int) request.getAttribute("curUserId");
-        Child child = childService.registerChild(childDto, curUserId);
-        if (child != null){
-            return new ResponseEntity<>(SUCCESS, HttpStatus.OK);
-        } else {
-            return new ResponseEntity<>(FAIL, HttpStatus.FORBIDDEN);
-        }
+        Child data = childService.registerChild(childDto, curUserId);
+        return new ResponseEntity<>(data, HttpStatus.OK);
     }
 
     @ApiOperation(value = "닉네임 중복 확인", notes = "유저 닉네임 받아 중복 확인")
@@ -72,9 +68,8 @@ public class MemberController {
 
     @ApiOperation(value = "아이 닉네임 수정", notes = "아이 nickname 수정 후 아이 프로필 정보 전달")
     @PutMapping("/child/{childId}")
-    public ResponseDto updateProfile(@PathVariable int childId, @RequestBody ChildUpdateDto childDto) {
-        ChildProfileDto data = childService.updateProfile(childId, childDto);
-        return new ResponseDto(data);
+    public ResponseEntity<ChildProfileDto> updateProfile(@PathVariable int childId, @RequestBody ChildUpdateDto childDto) {
+        return new ResponseEntity<>(childService.updateProfile(childId, childDto), HttpStatus.OK);
     }
 
     @ApiOperation(value = "회원 탈퇴", notes = "토큰 ID 받아서 회원 탈퇴")
--- BE/greenEarth/src/main/java/com/ssafy/greenEarth/exception/ErrorCode.java
+++ BE/greenEarth/src/main/java/com/ssafy/greenEarth/exception/ErrorCode.java
@@ -16,8 +16,10 @@ public enum ErrorCode {
 
     NOT_EXIST_ACCOUNT(NOT_FOUND, "A001", "계정이 존재하지 않습니다."),
     INVALID_ACCOUNT(FORBIDDEN, "A002", "이메일 혹은 비밀번호가 일치하지 않습니다."),
+
     NOT_EXIST_MISSION(NOT_FOUND, "M001", "미션이 존재하지 않습니다."),
     NOT_EXIST_MISSION_LOG(NOT_FOUND, "M002", "미션 로그가 존재하지 않습니다."),
+
     NOT_EXIST_REWARD(NOT_FOUND, "ROO1", "보상이 존재하지 않습니다.");
 
     private final HttpStatus httpStatus;
