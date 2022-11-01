11938bf227edee443a3d7efd50fc938ae018fe27
Jieun <skysun704@gmail.com>
2022-11-01 15:32:38 +0900

    [S07P31D206-358] cors setting
    Former-commit-id: c9362aa61181c2d129a2ccd3c2f063e07310a7c3

--- BE/greenEarth/src/main/java/com/ssafy/greenEarth/repository/GreenEarthRepository.java
+++ BE/greenEarth/src/main/java/com/ssafy/greenEarth/repository/GreenEarthRepository.java
@@ -5,11 +5,12 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
+import javax.persistence.EntityManager;
 import java.util.Optional;
 
 public interface GreenEarthRepository extends JpaRepository<GreenEarth, Integer> {
 
-    @Query("select g from GreenEarth g where g.mileage_condition >= :mileage")
+    @Query("select g from GreenEarth g where g.mileage_condition <= :mileage")
     Optional<GreenEarth> findFirstByMileage_condition(@Param("mileage") int mileage);
 
 }
