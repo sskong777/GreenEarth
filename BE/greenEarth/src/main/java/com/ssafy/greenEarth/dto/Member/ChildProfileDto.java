699c48f3d8ad495901765e4407b7de23dcfbf70d
jongil512 <jongil519@gmail.com>
2022-11-02 16:17:07 +0900

    refactor - DTO Annotaion refactoring
    Former-commit-id: 7dd0574741da3fb784a9f5b9eb45c2d13401fa52

--- BE/greenEarth/src/main/java/com/ssafy/greenEarth/dto/Member/ChildProfileDto.java
+++ BE/greenEarth/src/main/java/com/ssafy/greenEarth/dto/Member/ChildProfileDto.java
@@ -12,43 +12,33 @@ import java.time.LocalDate;
 @Data
 public class ChildProfileDto {
 
-    @NotNull(message = "child_id를 입력해주세요")
     @Schema(description = "아이 PK")
     private int childId;
 
-    @NotBlank(message = "이름을 입력해주세요")
     @Schema(description = "아이 실명")
     private String realName;
 
-    @NotBlank(message = "닉네임을 입력해주세요")
     @Schema(description = "아이 닉네임(로그인 ID)")
     private String nickname;
 
-    @NotNull(message = "성별을 입력해주세요")
     @Schema(description = "아이 성별")
     private Gender gender;
 
-    @NotNull(message = "마일리지를 입력해주세요")
     @Schema(description = "아이 마일리지")
     private int mileage;
 
-    @NotNull(message = "클리어한 미션 개수를 입력해주세요")
     @Schema(description = "아이가 클리어한 미션 개수")
     private int clearedMission;
 
-    @NotNull(message = "지구 레벨을 입력해주세요")
     @Schema(description = "아이의 지구 레벨")
     private int earthLevel;
 
-    @NotNull(message = "생일을 입력해주세요")
     @Schema(description = "아이의 생일")
     private LocalDate birthday;
 
-    @NotNull(message = "아바타 번호를 입력해주세요")
     @Schema(description = "아이의 아바타 번호")
     private int avatar;
 
-    @NotBlank(message = "보호자 닉네임을 입력해주세요")
     @Schema(description = "아이와 연결된 보호자 닉네임")
     private String parent;
 
