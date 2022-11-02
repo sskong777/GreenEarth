61ae2b348538324a9158aafdefa6a1c0444ad8e4
jongil512 <jongil519@gmail.com>
2022-11-02 16:17:07 +0900

    refactor - DTO Annotaion refactoring
    Former-commit-id: 0600b83b0f3840ad9e633ea79f194c6f8dddc448

--- BE/greenEarth/src/main/java/com/ssafy/greenEarth/dto/Member/ParentProfileDto.java
+++ BE/greenEarth/src/main/java/com/ssafy/greenEarth/dto/Member/ParentProfileDto.java
@@ -12,19 +12,15 @@ import java.util.List;
 @Data
 public class ParentProfileDto {
 
-    @NotNull(message = "parent_id를 입력해주세요")
     @Schema(description = "보호자 PK")
     private int parentId;
 
-    @NotBlank(message = "이메일을 입력해주세요")
     @Schema(description = "보호자 이메일")
     private String email;
 
-    @NotBlank(message = "닉네임을 입력해주세요")
     @Schema(description = "보호자 닉네임")
     private String nickname;
 
-    @NotNull(message = "연결된 아이를 입력해주세요")
     @Schema(description = "보호자와 연결된 아이")
     private List<Child> childList;
 
