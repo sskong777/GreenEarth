2e07ba292ddf98f1f2b4a65b3140271c22cf880c
unknown <ict.cspark@gmail.com>
2022-11-05 07:09:42 +0900

    fe merge
    Former-commit-id: b1375b6001cfaa51a484115b22cf9173ee8932e4

--- FE/green-earth/src/functions/useAuthCallback.js
+++ FE/green-earth/src/functions/useAuthCallback.js
@@ -1,10 +1,16 @@
 import { useRecoilState } from "recoil";
 import { accessTokenState, refreshTokenState } from "../store/LoginStore";
+import {
+  logInTokenState,
+  memberInfoState,
+  childInfoState,
+} from "../store/atoms";
 
 import { useCommonCallback } from "./useCommonCallback";
 
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
+import { useEffect } from "react";
 
 export const useAuthCallback = () => {
   const navigate = useNavigate();
@@ -14,6 +20,17 @@ export const useAuthCallback = () => {
 
   const [refreshToken, setRefreshToken] = useRecoilState(refreshTokenState);
   const [accessToken, setAccessToken] = useRecoilState(accessTokenState);
+
+  const [logInToken, setLogInToken] = useRecoilState(logInTokenState);
+  const [memberInfo, setMemberInfo] = useRecoilState(memberInfoState);
+  const [childInfo, setChildInfo] = useRecoilState(childInfoState);
+
+  // 임시 코드 (토큰 기능 완료시 삭제)
+  useEffect(() => {
+    setLogInToken(
+      "eyJhbGciOiJIUzI1NiJ9.eyJJZCI6MiwiUm9sZSI6IlJPTEVfUEFSRU5UIiwiaWF0IjoxNjY3NDUxNDYwLCJzdWIiOiJhY2Nlc3NUb2tlbiIsImV4cCI6MTY2NzUzNzg2MH0.kCol2o_xOem1z-1bWGGVjIy18d9sMzm_AOKlkeoym_M"
+    );
+  }, []);
 
   const loginCallback = async (id, password) => {
     axios({
@@ -150,10 +167,56 @@ export const useAuthCallback = () => {
     return response;
   };
 
+  // 회원 정보 콜백 함수
+  const memberInfoCallback = async (token) => {
+    axios({
+      method: "get",
+      url: "/api/member",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: token ? `Bearer ${token}` : `Bearer ${logInToken}`,
+      },
+    })
+      .then((response) => {
+        if (response.data) {
+          setMemberInfo(response.data);
+          console.log("회원 정보가 조회되었습니다.");
+          console.log("memberInfo :", response.data);
+        }
+      })
+      .catch((error) => {
+        console.log(error.response.data);
+      });
+  };
+
+  // 아이 정보 콜백 함수
+  const childInfoCallback = async (childId) => {
+    axios({
+      method: "get",
+      url: `/api/member/child/${childId}`,
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${logInToken}`,
+      },
+    })
+      .then((response) => {
+        if (response.data) {
+          setChildInfo(response.data);
+          console.log("아이 정보가 조회되었습니다.");
+          console.log("childInfo :", response.data);
+        }
+      })
+      .catch((error) => {
+        console.log(error.response.data);
+      });
+  };
+
   return {
     loginCallback,
     kakaoLoginCallback,
     signUpCallback,
     nickNameCheckCallback,
+    memberInfoCallback,
+    childInfoCallback,
   };
 };
