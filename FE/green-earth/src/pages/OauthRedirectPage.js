9cfcde2b799b69b41bd2ec0eac1bdccebfae4365
GyeongYeol <hrq789@naver.com>
2022-11-07 13:59:21 +0900

    [S07P31D206-294] refactor - kakao 로그인 리다이렉트 변경
    Former-commit-id: 8979cf543a684ee6efc0be2d78a27bca75303475

--- FE/green-earth/src/pages/OauthRedirectPage.js
+++ FE/green-earth/src/pages/OauthRedirectPage.js
@@ -9,23 +9,12 @@ const OauthRedirectPage = () => {
   const params = new URLSearchParams(location.search);
   const code = params.get("code");
 
-  //   useEffect(() => {
-  //     console.log(code);
-  //     kakaoLoginCallback(code);
-  //   }, []);
-
-  const event = () => {
-    // console.log(typeof code);
+  useEffect(() => {
+    console.log(code);
     kakaoLoginCallback(code);
-  };
+  }, []);
 
-  return (
-    <div>
-      리아이렉트
-      <div>안녕하세요</div>
-      <button onClick={event}>{code}</button>
-    </div>
-  );
+  return <div>카카오 회원 가입 중입니다...</div>;
 };
 
 export default OauthRedirectPage;
