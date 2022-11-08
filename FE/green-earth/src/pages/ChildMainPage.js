c69dabfc3bfe31ecff83eda331ae0e810b1efca1
강지명 <rainbow7417@naver.com>
2022-11-08 11:14:33 +0900

    fix - CloneErrorFix
    Former-commit-id: 48309488a0dc9dd540e44d164d9b4036e3cf45a9
    Former-commit-id: f3e467e1d3650360bf60c8476e49c77ef7814ebe

--- FE/green-earth/src/pages/ChildMainPage.js
+++ FE/green-earth/src/pages/ChildMainPage.js
@@ -1,34 +1,8 @@
 
 import "../style/ChildMain.css";
-// import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { Link } from "react-router-dom";
-import axios from 'axios'; // 액시오스
-// import Home from "./pages/Home";
-import {useRef} from 'react'
-//import {useState} from 'react'
-import {Suspense } from 'react'
-import {Canvas} from '@react-three/fiber'
-import {OrbitControls, useGLTF,useAnimations } from '@react-three/drei'
-import Mission from '../components/ChildMainPage/Mission.js'
-import rocket from '../image/rocket.png'
-import start from '../image/start.png'
-import end from '../image/end.png'
-import { Color } from "three";
-import {useEffect, useState} from "react";
 
-import {Child1, Child2, Child3, Child4, Child5, Child6, Child7, Child8, Child9, Child10, 
-  Child11, Child12, Child13, Child14, Child15, Child16, Child17, Child18, Child19, Child20, 
-  Child21, Child22, Child23, Child24, Child25, Child26, Child27, Child28, Child29, Child30,
-  Child31, Child32, Child33, Child34, Child35, Child36, Child37, Child38, Child39, Child40, 
-  Child41, Child42, Child43, Child44, Child45, Child46, Child47, Child48, Child49, Child50,
-  Child51, Child52, Child53, Child54, Child55} from "../Model.js/Child"
 
-import {Model9, Model10, Model11, Model12, Model13, Model14, Model15, Model16, Model17, Model18, Model19
-, Model90, Model100, Model110, Model120, Model130, Model140, Model150, Model160, Model170, Model180, Model190, Model20} from "../Model.js/Badge"
 
-import { Model3, Model, Model0, Model00, Model000} from "../Model.js/Space"
-
-import {Beluga} from "../Model.js/Animal"
 
 import {Earth1} from "../components/ChildMainPage/Earth1"
 import {Earth2} from "../components/ChildMainPage/Earth2"
@@ -42,12 +16,7 @@ import {Earth8} from "../components/ChildMainPage/Earth8"
 
 function ChildMainPage() {
   
-  const level = 1
-
-
-  // useEffect(() => {
-  //     window.location.reload();
-  // }, []); //두 번째 인수로 빈 배열을 준다.
+  const level = 8
 
 
   if (level ===1 ){
