6b6c429018fdfd19c48f7d2133d053e56ad791c7
3jieun3 <skysun704@gmail.com>
2022-11-10 14:05:05 +0900

    refactor - code analysis result refactoring
    Former-commit-id: 98a9ae7ca4f7dd49652e7b39101c6342f9b7925d
    Former-commit-id: 13e98ee0b4f13462703afa45fb4893620367d4d9

--- BE/greenEarth/src/main/java/com/ssafy/greenEarth/repository/RewardRepository.java
+++ BE/greenEarth/src/main/java/com/ssafy/greenEarth/repository/RewardRepository.java
@@ -1,12 +1,11 @@
 package com.ssafy.greenEarth.repository;
 
-import com.ssafy.greenEarth.domain.Child;
 import com.ssafy.greenEarth.domain.Reward;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
 
-public interface RewardRespository extends JpaRepository<Reward, Integer> {
+public interface RewardRepository extends JpaRepository<Reward, Integer> {
 
     Optional<Reward> findRewardById(int id);
 
