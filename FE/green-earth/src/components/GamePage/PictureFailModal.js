c89dc0c3a83b6ee5621d4439f0dfb3ccf7f39249
GyeongYeol <hrq789@naver.com>
2022-11-14 17:59:51 +0900

    [S07P31D206-453] feature - 틀린그림찾기 결과 모달 작성
    Former-commit-id: 53a8b9701184239b639f74a8cb1f44c0c99c2241
    Former-commit-id: 457b2f18ea1243d39df86634131d88d8e7ec4a01

---
+++ FE/green-earth/src/components/GamePage/PictureFailModal.js
@@ -0,0 +1,58 @@
+import { useNavigate } from "react-router-dom";
+
+import "animate.css";
+
+const PictureFailModal = ({ setnowPage, setLife }) => {
+  const navigate = useNavigate();
+
+  // 게임 다시하기 버튼
+  const handleClickCardsPairingButton = (e) => {
+    setnowPage("main");
+    setLife(3);
+  };
+
+  // 게임 홈화면 이동 버튼
+  const handleClickGameLandButton = (e) => {
+    navigate(0);
+  };
+
+  return (
+    <>
+      <div className="CardsPairingModal">
+        <div className="CardsPairingModalBody">
+          <div className="CardsPairingModalContent py-3 my-5">
+            <div className="CardsPairingModalContent">
+              <div className="CardsPairingModalTitle">실패했습니다...</div>
+              <div className="CardsPairingModalHeader">
+                {/* 도전 횟수 안내 */}
+                <div className="CardsPairingModalInfo">
+                  <div className="text-3xl text-[#070803]">
+                    🔥 다시 도전해볼까요?
+                  </div>
+                </div>
+              </div>
+              <hr />
+            </div>
+            <hr />
+            <div className="mt-12">
+              <button
+                className="CardsPairingModalButtonClose"
+                onClick={handleClickCardsPairingButton}
+              >
+                다시하기
+              </button>
+              <button
+                className="CardsPairingModalButton"
+                onClick={handleClickGameLandButton}
+              >
+                게임랜드
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default PictureFailModal;
