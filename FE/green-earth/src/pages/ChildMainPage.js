b8aa1e4aaf7975035b4cf63e7065d3efbbce7bbd
ict-cspark <ict.cspark@gmail.com>
2022-11-14 16:25:22 +0900

    conflit fix
    Former-commit-id: eb8ec1cf8b70db4d3d30c195f7ba149ef3558f95
    Former-commit-id: ca592614c7747a676d0c458b31d17e1965ef5880

--- FE/green-earth/src/pages/ChildMainPage.js
+++ FE/green-earth/src/pages/ChildMainPage.js
@@ -1,13 +1,18 @@
+
 import "../style/ChildMainPage/ChildMain.css";
 
-import { Earth1 } from "../components/ChildMainPage/Earth1";
-import { Earth2 } from "../components/ChildMainPage/Earth2";
-import { Earth3 } from "../components/ChildMainPage/Earth3";
-import { Earth4 } from "../components/ChildMainPage/Earth4";
-import { Earth5 } from "../components/ChildMainPage/Earth5";
-import { Earth6 } from "../components/ChildMainPage/Earth6";
-import { Earth7 } from "../components/ChildMainPage/Earth7";
-import { Earth8 } from "../components/ChildMainPage/Earth8";
+
+
+
+import {Earth1} from "../components/ChildMainPage/Earth1"
+import {Earth2} from "../components/ChildMainPage/Earth2"
+import {Earth3} from "../components/ChildMainPage/Earth3"
+import {Earth4} from "../components/ChildMainPage/Earth4"
+import {Earth5} from "../components/ChildMainPage/Earth5"
+import {Earth6} from "../components/ChildMainPage/Earth6"
+import {Earth7} from "../components/ChildMainPage/Earth7"
+import {Earth8} from "../components/ChildMainPage/Earth8"
+
 
 import { useNavigate, useParams } from "react-router-dom";
 import { useState, useEffect } from "react";
@@ -16,69 +21,83 @@ import { memberInfoState, childInfoState } from "../store/atoms";
 import { useAuthCallback } from "./../functions/useAuthCallback";
 
 function ChildMainPage() {
-  // Recoil에 저장되어 있는 아이정보, 회원정보, 로그인토큰 불러오기
-  const [memberInfo, setMemberInfo] = useRecoilState(memberInfoState);
+  
+    const navigate = useNavigate();
 
-  // 회원정보, 아이정보 Axios 요청
-  const { memberInfoCallback } = useAuthCallback();
+    // Recoil에 저장되어 있는 아이정보, 회원정보, 로그인토큰 불러오기
+    const [memberInfo, setMemberInfo] = useRecoilState(memberInfoState);
 
-  const { logoutcallback } = useAuthCallback();
 
-  // 페이지에 접근하면 해당 아이정보 Axios 요청
-  useEffect(() => {
-    memberInfoCallback();
-    console.log(memberInfo);
-  }, []);
+    // 회원정보, 아이정보 Axios 요청
+    const { memberInfoCallback } = useAuthCallback();
 
-  if (memberInfo.isParent === false && memberInfo.earthLevel === 1) {
+    const { logoutcallback } = useAuthCallback();
+
+    // 페이지에 접근하면 해당 아이정보 Axios 요청
+    useEffect(() => {
+      memberInfoCallback();
+      console.log(memberInfo)
+      }, []);
+
+
+
+
+  if (memberInfo.isParent === false && memberInfo.earthLevel ===1 ){
+    
+  return (
+    <div>
+      <Earth1 />
+    </div>
+    
+  );} 
+  else if (memberInfo.isParent === false &&  memberInfo.earthLevel === 2) {
     return (
-      <div>
-        <Earth1 />
-      </div>
-    );
-  } else if (memberInfo.isParent === false && memberInfo.earthLevel === 2) {
+    <div>
+      <Earth2 />
+    </div>
+  
+  );}
+  else if ( memberInfo.isParent === false && memberInfo.earthLevel === 3) {
     return (
-      <div>
-        <Earth2 />
-      </div>
-    );
-  } else if (memberInfo.isParent === false && memberInfo.earthLevel === 3) {
-    return (
-      <div>
-        <Earth3 />
-      </div>
-    );
-  } else if (memberInfo.isParent === false && memberInfo.earthLevel === 4) {
-    return (
+    <div>
+      <Earth3 />
+    </div>
+      
+  );}
+  else if (memberInfo.isParent === false &&  memberInfo.earthLevel === 4) {
+      return (
       <div>
         <Earth4 />
       </div>
-    );
-  } else if (memberInfo.isParent === false && memberInfo.earthLevel === 5) {
-    return (
+      
+  );}
+  else if (memberInfo.isParent === false &&  memberInfo.earthLevel === 5) {
+  return (
       <div>
         <Earth5 />
       </div>
-    );
-  } else if (memberInfo.isParent === false && memberInfo.earthLevel === 6) {
-    return (
+      
+); }
+  else if (memberInfo.isParent === false &&  memberInfo.earthLevel === 6) {
+  return (
       <div>
         <Earth6 />
       </div>
-    );
-  } else if (memberInfo.isParent === false && memberInfo.earthLevel === 7) {
-    return (
-      <div>
-        <Earth7 />
-      </div>
-    );
-  } else if (memberInfo.isParent === false && memberInfo.earthLevel === 8) {
-    return (
-      <div>
-        <Earth8 />
-      </div>
-    );
+  );}
+  else if (memberInfo.isParent === false &&  memberInfo.earthLevel === 7) {
+  return (
+    <div>
+      <Earth7 />
+    </div>
+  );}
+  else if (memberInfo.isParent === false && memberInfo.earthLevel === 8) {
+      return (
+    <div>
+      <Earth8 />
+    </div>
+  );}
+
   }
-}
+
 
 export default ChildMainPage;
