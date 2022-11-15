c5dbca2f2cd54f5511ccc073e797184f2d1c8b4c
SSAFY <sskong777@gmail.com>
2022-11-15 15:45:20 +0900

    chatting pjt
    Former-commit-id: 30b6e6c9eb34ee8aa148a403f6066a4628693ad2
    Former-commit-id: 219cf214b3ead5928836f78a25c761f615145e8b

---
+++ chatting/src/test/java/com/greenEarth/chatting/ChattingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.greenEarth.chatting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChattingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
