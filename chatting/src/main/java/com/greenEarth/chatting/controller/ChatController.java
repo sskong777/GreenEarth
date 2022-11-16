bcc61230d099d3e1fb9fe92ad66d49da7185a4cf
Jieun <skysun704@gmail.com>
2022-11-16 13:54:42 +0900

    [S07P31D206-398] feat - join notice producer logic
    Former-commit-id: f3398a20ea0f01be9ce30e167a464483f8723d98
    Former-commit-id: 2ad8784ef3e259bd91baed0bda21aed0336e44cf

---
+++ chatting/src/main/java/com/greenEarth/chatting/controller/ChatController.java
@@ -0,0 +1,25 @@
+package com.greenEarth.chatting.controller;
+
+import com.greenEarth.chatting.dto.ChatNotice;
+import com.greenEarth.chatting.service.ChatProducer;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.stereotype.Controller;
+
+@Slf4j
+@RequiredArgsConstructor
+@Controller
+public class ChatController {
+
+    @Value("${spring.kafka.template.default-topic}")
+    private String KAFKA_TOPIC;
+
+    private final ChatProducer ChatProducer;
+
+    @MessageMapping("/notice")
+    public void sendJoinNotice(ChatNotice notice) {
+        ChatProducer.sendJoinNotice(this.KAFKA_TOPIC, notice);
+    }
+}
