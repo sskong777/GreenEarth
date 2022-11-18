bc4dac5281196a50f19fa582929c84fa6d26bc2d
rainbow7417 <rainbow7417@naver.com>
2022-11-18 15:41:42 +0900

    test - renderSpeedTest13
    Former-commit-id: 94bfd2f904074824d6a183e46a2eb71eb176cf11
    Former-commit-id: 2198c49f6ea4a6dede836fef5624d1f6061c4a53

--- FE/green-earth/src/components/ChildMainPage/Earth1.js
+++ FE/green-earth/src/components/ChildMainPage/Earth1.js
@@ -7,25 +7,21 @@ import { Link } from "react-router-dom";
 
 import {Suspense } from 'react'
 import {Canvas} from '@react-three/fiber'
-import {OrbitControls, useGLTF,useAnimations } from '@react-three/drei'
+import {OrbitControls } from '@react-three/drei'
 import Mission from '../../components/ChildMainPage/Mission.js'
 import rocket from '../../image/rocket.png'
 import start from '../../image/start.png'
 import end from '../../image/end.png'
 
-import {useEffect, useState} from "react";
+import {useEffect} from "react";
 
-import { useNavigate, useParams } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import { useRecoilState } from "recoil";
 import { memberInfoState } from "../../store/atoms";
 import { useAuthCallback } from "./../../functions/useAuthCallback";
 
-import {Child1, Child2, Child3, Child4, Child5, Child6, Child7, Child8, Child9, Child10, 
-  Child11, Child12, Child13, Child14, Child15, Child16, Child17, Child18, Child19, Child20, 
-  Child21, Child22, Child23, Child24, Child25, Child26, Child27, Child28, Child29, Child30,
-  Child31, Child32, Child33, Child34, Child35, Child36, Child37, Child38, Child39, Child40, 
-  Child41, Child42, Child43, Child44, Child45, Child46, Child47, Child48, Child49, Child50,
-  Child51, Child52, Child53, Child54, Child55} from "../../Model.js/Child"
+import {Child1, Child2, Child3, Child4, Child5, Child6, Child7, Child8, Child10, Child15, Child30,
+  Child31, Child32, Child33, Child34, Child35, Child36, Child37, Child38, Child39} from "../../Model.js/Child"
 
 
 import { Model0, Model00} from "../../Model.js/Space"
