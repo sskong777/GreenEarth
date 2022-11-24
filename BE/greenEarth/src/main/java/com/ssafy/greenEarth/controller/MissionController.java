7c299febe2fd391f7d2f45256352e2160bf7cdd0
jongil512 <jongil519@gmail.com>
2022-11-24 12:36:57 +0900

    refactor - RequestMapping (Mission, Reward)
    Former-commit-id: 9d734aa6b90fe7b9185a600e66820fbb4ac0acd9
    Former-commit-id: 5b4a38ea5fd1fd12c58decb9c1ae64824470c915

--- BE/greenEarth/src/main/java/com/ssafy/greenEarth/controller/MissionController.java
+++ BE/greenEarth/src/main/java/com/ssafy/greenEarth/controller/MissionController.java
@@ -18,7 +18,7 @@ import java.util.List;
 @Slf4j
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("mission")
+@RequestMapping("/mission")
 public class MissionController {
 
     private final MissionService missionService;
